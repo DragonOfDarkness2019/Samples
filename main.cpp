95a3a0a85b4ac796e2c59291ab80b305d5e75d8a
Larry Isen <46800443+LarryIsen@users.noreply.github.com>
2019-06-01 02:41:07 +0200

    Add files via upload
    Updating files.  Some bugfixes.  New main.cpp for Jewel.h

---
+++ main.cpp
@@ -0,0 +1,74 @@
+// #include <Carbon/Carbon.h> // If opened on XCode on a Mac, uncomment
+#include <Jewel.h>
+
+//Variables
+CeJeweledPP playfield;
+
+int main(int argc, char* argv[])
+{
+   // IBNibRef 		nibRef;
+   // WindowRef 		window;
+    
+    //OSStatus		err;
+
+    // Create a Nib reference passing the name of the nib file (without the .nib extension)
+    // CreateNibReference only searches into the application bundle.
+   // err = CreateNibReference(CFSTR("main"), &nibRef);
+    //require_noerr( err, CantGetNibRef );
+    
+    // Once the nib reference is created, set the menu bar. "MainMenu" is the name of the menu bar
+    // object. This name is set in InterfaceBuilder when the nib is created.
+   // err = SetMenuBarFromNib(nibRef, CFSTR("MenuBar"));
+   // require_noerr( err, CantSetMenuBar );
+    
+    // Then create a window. "MainWindow" is the name of the window object. This name is set in 
+    // InterfaceBuilder when the nib is created.
+   // err = CreateWindowFromNib(nibRef, CFSTR("MainWindow"), &window);
+    //require_noerr( err, CantCreateWindow );
+
+    // We don't need the nib reference anymore.
+    //DisposeNibReference(nibRef);
+    
+    // The window was created hidden so show it.
+   // ShowWindow( window );
+    
+    // Call the event loop
+	
+		playfield=playfield.SetUpBoard();
+		
+		printf("\n");
+		playfield.DisplayBoard(playfield);
+		printf("\n");
+		
+		playfield=playfield.SwapPieces(playfield,0,0,5,5);
+		
+		printf("\n");
+		playfield.DisplayBoard(playfield);
+		printf("\n");
+		
+		playfield=playfield.RemoveJewel(playfield,5,0,1);
+		
+		printf("\n");
+		playfield.DisplayBoard(playfield);
+		int tempScore;
+		tempScore=playfield.ScoreValue();
+		printf("Score: %d", tempScore);
+		printf("\n");
+		
+		playfield=playfield.RemoveJewel(playfield,3,2,1);
+		
+		printf("\n");
+		playfield.DisplayBoard(playfield);
+		tempScore=playfield.ScoreValue();
+		printf("Score: %d", tempScore);
+		printf("\n");
+	
+		RunApplicationEventLoop();
+
+		CantCreateWindow:
+		CantSetMenuBar:
+		CantGetNibRef:
+	
+	return 0 ; err;
+}
+
